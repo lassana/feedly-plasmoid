5b52393b2ca6c2eacb0c1c41fe938acb6c1073b2
Nikolai Doronin <lassana.nd@gmail.com>
2016-02-11 22:16:43 +0300

    Use plasmoidviewer instead plasmawindowed.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 plasmapkg2 --remove com.github.lassana.feedly-plasmoid
 plasmapkg2 --install plasmoid
-plasmawindowed com.github.lassana.feedly-plasmoid
+plasmoidviewer -a com.github.lassana.feedly-plasmoid
